e41f384448ea8c7be61d1e9583791b6d006ebea2
Jean Stuardo <jea.stuardo@duocuc.cl>
2025-04-17 03:38:17 -0400

    change to django

--- mysite/models.py
+++ mysite/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Construccion(models.Model):
     nombre = models.CharField(max_length=100)
-    imagen = models.ImageField(upload_to='construcciones/', null=True, blank=True)
+    imagen = models.CharField(max_length=200)  # usa CharField si son rutas estáticas
     materiales = models.TextField(help_text="Separados por comas")
     descripcion = models.TextField()
 
@@ -12,4 +12,59 @@ class Construccion(models.Model):
 
     def __str__(self):
         return self.nombre
+
       
+
+class Animal(models.Model):
+    nombre = models.CharField(max_length=100)
+    hostilidad = models.CharField(max_length=100)  # ← corregido aquí
+    descripcion = models.TextField()
+    imagen = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.nombre
+
+class Planta(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    imagen = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Enemigo(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    imagen = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Pueblo(models.Model):
+    nombre = models.CharField(max_length=100)
+    imagen = models.CharField(max_length=200)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class UbicacionEspecifica(models.Model):
+    nombre = models.CharField(max_length=100)
+    imagen = models.CharField(max_length=200)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class UbicacionVariada(models.Model):
+    nombre = models.CharField(max_length=100)
+    imagen = models.CharField(max_length=200)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
